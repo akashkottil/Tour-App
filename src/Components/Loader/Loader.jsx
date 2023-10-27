49e4a345476777765175dee69116ab6e3b33b434
Akash Kottil <kottilakash@gmail.com>
2023-10-27 21:08:40 +0530

    First Commmit

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loader.css'
+
+const Loader = () => {
+  return (
+    <div style={{display:'flex', justifyContent:'center'}}>
+      <div class="loader"></div>
+    </div>
+  )
+}
+
+export default Loader
